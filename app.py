6ea210f45450860c98be0b8111aa5ed7c3efe4b0
Kimberly <mrkid863@gmail.com>
2024-08-10 17:18:43 +0800

    use while loop to post to all clubs

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.driver_cache import DriverCacheManager
 from dotenv import load_dotenv
 import time
-import random
 import os
 import sys
 import logging
@@ -62,55 +61,59 @@ logger.info('POST_URLS: ' + str(post_url_list))
 def is_frozen():
     return getattr(sys, 'frozen', False)
 
+driver = None
+def open_browser():
+    global driver
 
-# set the path for webdriver
-if is_frozen():
-    # if running in a PyInstaller bundle
-    wdm_driver_root_path = sys._MEIPASS
-else:
-    # if running in a regular Python environment
-    wdm_driver_root_path = os.getcwd()
-service = ChromeService(executable_path=ChromeDriverManager(cache_manager=DriverCacheManager(root_dir=wdm_driver_root_path)).install())
+    # set the path for webdriver
+    if is_frozen():
+        # if running in a PyInstaller bundle
+        wdm_driver_root_path = sys._MEIPASS
+    else:
+        # if running in a regular Python environment
+        wdm_driver_root_path = os.getcwd()
+    service = ChromeService(executable_path=ChromeDriverManager(cache_manager=DriverCacheManager(root_dir=wdm_driver_root_path)).install())
 
-# disable notification
-chrome_options = webdriver.ChromeOptions()
-prefs = {"profile.default_content_setting_values.notifications": 2}
-chrome_options.add_experimental_option("prefs", prefs)
-if hide_browser:
-    chrome_options.add_argument("--headless")
+    # disable notification
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"profile.default_content_setting_values.notifications": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
+    if hide_browser:
+        chrome_options.add_argument("--headless")
 
-logger.info('開啟瀏覽器')
-driver = webdriver.Chrome(service=service, options=chrome_options)
-time.sleep(5)
+    logger.info('開啟瀏覽器')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    time.sleep(5)
 
-logger.info('開啟臉書首頁')
-driver.get("http://www.facebook.com")
-time.sleep(5)
-assert "Facebook" in driver.title
+def login():
+    logger.info('開啟臉書首頁')
+    driver.get("http://www.facebook.com")
+    time.sleep(5)
+    assert "Facebook" in driver.title
 
-logger.info('輸入帳號')
-driver.find_element(by=By.ID, value='email').send_keys(user_email)
-time.sleep(3)
+    logger.info('輸入帳號')
+    driver.find_element(by=By.ID, value='email').send_keys(user_email)
+    time.sleep(3)
 
-logger.info('輸入密碼')
-driver.find_element(by=By.ID, value='pass').send_keys(user_password)
-time.sleep(1)
+    logger.info('輸入密碼')
+    driver.find_element(by=By.ID, value='pass').send_keys(user_password)
+    time.sleep(1)
 
-logger.info('按下登入')
-driver.find_element(by=By.NAME, value='login').click()
-time.sleep(10)
+    logger.info('按下登入')
+    driver.find_element(by=By.NAME, value='login').click()
+    time.sleep(10)
 
-logger.info('切換帳號')
-driver.find_element(by=By.XPATH, value='//div[@aria-label="你的個人檔案"]').click()
-time.sleep(8)
+    logger.info('切換帳號')
+    driver.find_element(by=By.XPATH, value='//div[@aria-label="你的個人檔案"]').click()
+    time.sleep(8)
 
-logger.info('選擇要切換的帳號: ' + login_switch_account)
-driver.find_element(by=By.XPATH, value='//div[@aria-label="切換為' + login_switch_account + '"]').click()
-time.sleep(8)
+    logger.info('選擇要切換的帳號: ' + login_switch_account)
+    driver.find_element(by=By.XPATH, value='//div[@aria-label="切換為' + login_switch_account + '"]').click()
+    time.sleep(8)
 
 
-# prepare post
-def prepare_post(post_url: str, club_index: int = None, club_keyword: str = None):
+# show club for post
+def show_club_for_post(post_url: str, club_index: int):
     logger.info('切換到想發的文章: ' + post_url)
     driver.get(post_url)
     time.sleep(10)
@@ -124,102 +127,124 @@ def prepare_post(post_url: str, club_index: int = None, club_keyword: str = None
     time.sleep(8)
 
     # starting to move down to have all clubs shown up
-    if club_keyword is None:
-        actions = ActionChains(driver)
-        while True:
-            listitem_elements = driver.find_elements(By.XPATH, '//div[@role="listitem"]')
-            before_moveto_count = len(listitem_elements)
-            if (before_moveto_count == 0):
-                logger.info('找不到社團')
-                break
-            elif club_index is not None and before_moveto_count > club_index:
-                logger.info('已顯示目標社團，不需再往下移動')
-                break
-            else:
-                logger.info('往下移之前有 ' + str(before_moveto_count) + " 個社團")
-            
+    actions = ActionChains(driver)
+    while True:
+        logger.info('尋找目標社團index: ' + str(club_index))
+        club_list = driver.find_elements(By.XPATH, '//div[@role="listitem"]')
+        before_moveto_count = len(club_list)
+        if (before_moveto_count == 0):
+            logger.info('找不到任何社團')
+            return False
+        elif before_moveto_count > club_index:
+            logger.info('已顯示目標社團index: ' + str(club_index) +'，不需再往下移動')
+            return True
+        else:
+            logger.info('往下移之前有 ' + str(before_moveto_count) + " 個社團")
             logger.info('移至最下面的社團以取得更多社團清單')
-            actions.move_to_element(listitem_elements[before_moveto_count-1]).perform()
+            actions.move_to_element(club_list[before_moveto_count-1]).perform()
             time.sleep(3)
             new_count = len(driver.find_elements(By.XPATH, '//div[@role="listitem"]'))
             logger.info('往下移之後有 ' + str(new_count) + " 個社團")
             if new_count == before_moveto_count:
                 logger.info('不需再往下移')
-                break
+                return False
             else:
-                logger.info('還要再往下移')
                 continue
-    else:
-        # search club by keyword
-        logger.info('搜尋社團: ' + club_keyword)
-        driver.find_element(by=By.XPATH, value='//input[@aria-label="搜尋社團"]').send_keys(club_keyword)
-        time.sleep(3)
 
 
 # perform post
-def perform_post(club_element, expected_club_text: str):
-    actual_club_text = club_element.text.split('\n', 1)[0].strip()
-    # check actual club is as expected
-    if actual_club_text != expected_club_text:
-        logger.warning('社團: ' + actual_club_text + ", 與預期社團不符: " + expected_club_text + ', 未能成功發佈: ' + post_url)
-        return False
-    elif len(actual_club_text) == 0:
-        logger.info('空社團名稱，不需發佈')
-        return False
-    elif actual_club_text in club_ignore_list:
-        logger.info('此社團已被列入社團忽略清單，不需發佈: ' + actual_club_text)
-        return False
-
+def perform_post(club_element, club_text: str, post_url: str):
     actions = ActionChains(driver)
-    logger.info('移至社團，準備操作: ' + actual_club_text)
+    logger.info('移至社團: ' + club_text)
     actions.move_to_element(club_element).perform()
     time.sleep(2)
     
-    logger.info('點擊社團: ' + actual_club_text)
+    logger.info('點擊社團: ' + club_text)
     club_element.click()
     time.sleep(8)
 
     logger.info('輸入留言: ' + post_message)
-    try:
-        driver.find_element(by=By.XPATH, value='//div[@aria-label="留個言吧......"]').send_keys(post_message)
-    except:
-        driver.find_element(by=By.XPATH, value='//div[@aria-label="建立公開貼文……"]').send_keys(post_message)
+    driver.find_element(by=By.XPATH, value='//div[contains(@class,"notranslate") and @aria-describedby]').send_keys(post_message)
     time.sleep(5)
 
     # only in normal mode will publish actually
     if mode == "normal":
         driver.find_element(by=By.XPATH, value='//div[@aria-label="發佈"]').click()
-        logger.info('已發佈: ' + actual_club_text + ", 文章: " + post_url)
+        logger.info('社團: ' + club_text + ", 已發佈: " + post_url)
     else:
-        logger.info('未發佈，以測試訊息替代: ' + actual_club_text + ", 文章: " + post_url)
-
-    return True
+        logger.info('社團: ' + club_text + ", 以此測試訊息替代發佈: " + post_url)
 
 
-for post_url in post_url_list:
-    try:
-        prepare_post(post_url)
-        club_elements = driver.find_elements(by=By.XPATH, value='//div[@role="listitem"]')
-        club_count = len(club_elements)
-        club_text_list = [element.text.split('\n', 1)[0].strip() for element in club_elements]
-        logger.info('共找到 ' + str(club_count) + ' 個社團')
-        time.sleep(5)
-    except Exception as e:
-        logger.warning(f"捕捉到錯誤: {e}")
-        logger.warning('全社團未能成功發文: ' + post_url)
-        continue
+def start_to_post():
+    for post_url in post_url_list:
+        club_count = 0
+        club_text_list = None
+        posted_club_list = []
+        failed_club_list = []
+        club_index_to_show = 0
+        no_club_found = False
+        no_more_club_to_post = False
+        while True:
+            try:
+                show_club_for_post(post_url, club_index_to_show)
+                club_elements = driver.find_elements(by=By.XPATH, value='//div[@role="listitem"]')
+                club_count = len(club_elements)
+                club_text_list = [element.text.split('\n', 1)[0].strip() for element in club_elements]
+                logger.info('共找到 ' + str(club_count) + ' 個社團')
 
-    for index in range(club_count):
-        club_text = club_text_list[index]
-        try:
-            if index != 0:  # prepare post again for 2nd club and after, as the page status is changed after posting one and need to search by club_text
-                prepare_post(post_url, index)
+                # found no club: check it again before breaking while loop
+                if club_count == 0:
+                    no_more_club_to_post = False
+                    if no_club_found:
+                        logger.info('連續2次空社團清單')
+                        break
+                    else:
+                        no_club_found = True
+                        continue
+                else:
+                    no_club_found = False
+                    if all((element in club_ignore_list or element in posted_club_list) for element in club_text_list if element):
+                        if no_more_club_to_post:
+                            logger.info('連續2次沒有找到需發佈文章的社團')
+                            break
+                        elif club_index_to_show < club_count:
+                            club_index_to_show = club_count
+                            logger.info('當前頁面社團已全發佈過，跳至index: ' + str(club_index_to_show))
+                            no_more_club_to_post = False
+                            continue
+                        else:
+                            no_more_club_to_post = True
+                            logger.info('當前頁面社團已全發佈過，且index: ' + str(club_index_to_show) + ' 已大於等於當前頁面社團總數')
 
-            club_element = driver.find_elements(by=By.XPATH, value='//div[@role="listitem"]')[index]
-            perform_post(club_element, club_text)
+                for index, element in enumerate(club_text_list):
+                    if element and element not in club_ignore_list and element not in posted_club_list:
+                        no_more_club_to_post = False
+                        club_index_to_show = index
+                        club_element = club_elements[index]
+                        club_text = club_text_list[index]
+                        try:
+                            perform_post(club_element, club_text, post_url)
+                            posted_club_list.append(club_text)
+                            club_index_to_show += 1
+                            time.sleep(10)
+                        except Exception as e:
+                            logger.warning(f"捕捉到錯誤: {e}")
+                            # mark club as posted if already in failed list
+                            if club_text in failed_club_list:
+                                posted_club_list.append(club_text)
+                                logger.warning('社團: ' + club_text + ', 最終未能成功發佈: ' + post_url)
+                            else:
+                                failed_club_list.append(club_text)
+                                logger.warning('社團: ' + club_text + ', 此次未能成功發佈: ' + post_url)
+                        break
+            except Exception as e:
+                logger.warning(f"捕捉到錯誤: {e}")
+                time.sleep(3)
 
-        except Exception as e:
-            logger.warning(f"捕捉到錯誤: {e}")
-            logger.warning('社團: ' + club_text + ', 未能成功發佈: ' + post_url)
-        finally:
-            time.sleep(random.randint(10, 15))
+        logger.info("結束發佈: " + post_url)
+        logger.info("需手動發佈: " + ", ".join(set(failed_club_list) & set(posted_club_list)))
+        logger.info("已自動發佈: " + ", ".join(set(posted_club_list) - set(failed_club_list)))
+
+open_browser()
+login()
+start_to_post()
